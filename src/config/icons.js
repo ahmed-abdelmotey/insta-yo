17dda6a228505a95c725c1d55b5bfe928a30ef89
ahmed-abdelmotey <ahmed.abdelmotey92@gmail.com>
2020-06-19 23:58:34 +0200

    feat: configure font awesome and install missing packages

---
+++ src/config/icons.js
@@ -0,0 +1,13 @@
+/**
+ * fortawesome icons used
+ * this is used for importing only icons needed
+ * to keep package size as small as possible
+ */
+
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+const usedIcons = [
+  faSearch,
+];
+
+export default usedIcons;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
 import App from './App.vue';
 import router from './router';
+import icons from './config/icons';
+
+// add icons used from fort awesome
+library.add(...icons);
+
+// register font awesome icon component
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
