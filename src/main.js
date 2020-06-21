28f5c74ffd919bf5cff5e07b7ed82ae0c666f3d5
ahmed-abdelmotey <ahmed.abdelmotey92@gmail.com>
2020-06-21 19:28:10 +0200

    feat: add i18n and localization config

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import App from './App.vue';
 import router from './router';
 import icons from './config/icons';
+import i18n from './i18n';
 
 // add icons used from fort awesome
 library.add(...icons);
@@ -15,5 +16,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
+  i18n,
   render: (h) => h(App),
 }).$mount('#app');
