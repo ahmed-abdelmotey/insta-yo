c6a82270d95e2c228e5d897f5d216b5fba6f284a
ahmed-abdelmotey <ahmed.abdelmotey92@gmail.com>
2020-06-23 01:01:03 +0200

    feat: add search box navigation

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,13 +1,33 @@
 <template>
     <div class="search">
         <input
+            @keyup.enter="search"
             type="text"
             name="search"
             id="search-input"
             class="search__input"
-            placeholder="Search">
-        <button class="search__button">
+            placeholder="Search"
+            v-model="searchValue">
+        <button class="search__button" @click="search">
             <font-awesome-icon :icon="['fa', 'search']" />
         </button>
     </div>
 </template>
+
+<script>
+export default {
+  name: 'SearchInput',
+  data() {
+    return {
+      searchValue: '',
+    };
+  },
+  methods: {
+    search() {
+      if (this.$route.name !== 'Home' || !this.$route.query.query || this.$route.query.query !== this.searchValue) {
+        this.$router.push({ name: 'Home', query: { query: this.searchValue } });
+      }
+    },
+  },
+};
+</script>
